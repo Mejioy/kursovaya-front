b767c1556c3242fd86cd095fe6d329670152869d
Кокурин Ярослав <yaroslav.kokurin@mail.ru>
2024-11-27 00:29:53 +0300

    Initial commit

---
+++ src/main/java/ru/kafpin/lr6_bzz/controllers/EditAutomobileController.java
@@ -0,0 +1,68 @@
+package ru.kafpin.lr6_bzz.controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import ru.kafpin.lr6_bzz.domains.Automobile;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class EditAutomobileController {
+    private Automobile automobile;
+    public void setEditStage(Stage editStage) {
+        this.editStage = editStage;
+    }
+    private Stage editStage;
+    private ResourceBundle bundle;
+    public void setBundle(ResourceBundle bundle) {
+        this.bundle = bundle;
+    }
+    public boolean isAction() {
+        return action;
+    }
+    private boolean action=false;
+
+    public void setAutomobile(Automobile automobile) {
+        this.automobile = automobile;
+        tMark.setText(automobile.getMark());
+        tModel.setText(automobile.getModel());
+        tGosnumber.setText(automobile.getGosnumber());
+    }
+    @FXML
+    private TextField tGosnumber;
+    @FXML
+    private TextField tMark;
+    @FXML
+    private TextField tModel;
+    @FXML
+    void onCancel(ActionEvent event) {
+        editStage.close();
+    }
+    private void Error(String text){
+        Alert alert;
+        alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(bundle.getString("error"));
+        alert.setContentText(null);
+        alert.setHeaderText(text);
+        alert.showAndWait();
+    }
+    @FXML
+    void onOK(ActionEvent event) {
+        if (tMark.getText()==null||tMark.getText().trim().isEmpty())
+            Error(bundle.getString("markfieldempty"));
+        else if (tModel.getText()==null||tModel.getText().trim().isEmpty())
+            Error(bundle.getString("modelfieldempty"));
+        else if (tGosnumber.getText()==null||tGosnumber.getText().trim().isEmpty())
+            Error(bundle.getString("gosnumberfieldempty"));
+        else
+        {
+            automobile.setMark(tMark.getText().trim());
+            automobile.setModel(tModel.getText().trim());
+            automobile.setGosnumber(tGosnumber.getText().trim());
+            action=true;
+            editStage.close();
+        }
+    }
+}
---
+++ src/main/java/ru/kafpin/lr6_bzz/controllers/RemoveController.java
@@ -0,0 +1,26 @@
+package ru.kafpin.lr6_bzz.controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+public class RemoveController {
+    private Stage removeStage;
+    public void setRemoveStage(Stage removeStage) {
+        this.removeStage = removeStage;
+    }
+    private boolean remove=false;
+    public boolean isRemove() {
+        return remove;
+    }
+    public void setRemove(boolean remove) {
+        this.remove = remove;
+    }
+    @FXML
+    void onCancel(ActionEvent event) {
+        removeStage.close();
+    }
+    @FXML
+    void onOK(ActionEvent event) {
+        remove=true;
+        removeStage.close();
+    }
+}
